07f57e97ccf61863441aadde01c842206c157aff
Amarendra Singh <imamarendra.singh@gmail.com>
2023-02-09 19:24:16 +0530

    admin.py

--- foodswipe/settings.py
+++ foodswipe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -89,6 +90,8 @@ DATABASES = {
         'HOST':config('DB_HOST'),
     }
 }
+
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
