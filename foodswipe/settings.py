79a555ca2097bf537ff8c17319b5cd1d0bedf177
amarendra-singh <imamarendra.singh@gmail.com>
2023-03-03 17:05:16 +0530

    static config

--- foodswipe/settings.py
+++ foodswipe/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
         'USER':config('DB_USER'),
         'PASSWORD':config('DB_PASSWORD'),
         'HOST':config('DB_HOST'),
+        'PORT':config('DB_PORT'),
     }
 }
 
@@ -132,6 +133,9 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'Foodswipe-MultiVendor-Django/static'
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
