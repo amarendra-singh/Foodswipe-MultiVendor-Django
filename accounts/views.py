a31a272bf0df987c79b2dc2a76c0d7dd1adbdbf9
amarendra-singh <imamarendra.singh@gmail.com>
2023-12-10 19:08:32 +0530

    Implementation of Password Management, Vendor Dashboard, Vendor Approvel, Email Notification

--- accounts/views.py
+++ accounts/views.py
@@ -92,7 +92,9 @@ def registerVendor(request):
             vendor.save()
 
             #send verification email
-            send_verification_email(request,user)
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request,user,mail_subject,email_template)
 
             messages.success(request, "Your account has been registered successfully")
             return redirect('registerVendor')
@@ -151,6 +153,63 @@ def activate(request, uidb64, token):
         messages.error(request, 'Invalid activation link')
         return redirect('myaccount')
     
+def forgot_password(request):
+    if request.method=="POST":
+        email = request.POST['email']
+
+        if User.objects.filter(email=email).exists():
+            user=User.objects.get(email__exact=email)
+
+            mail_subject = "Reset your Password"
+            email_template = 'accounts/emails/password_reset_email.html'
+
+            send_verification_email(request, user,mail_subject,email_template)
+            messages.success(request, "Password reset link has been sent to your email address.")
+            return redirect('login')
+        else:
+            messages.error(request, "Account does not exist")
+            return redirect('forgot_password')
+
+    return render(request, 'accounts/forgot_password.html')
+
+def reset_password_validate(request,uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request,'Please reset your password')
+        return redirect('reset_password')
+    else:
+        messages.error(request,'This link has been expired')
+        return redirect('myaccount')
+
+
+def reset_password(request):
+    if request.method=='POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            pk = request.session.get('uid')
+
+            user = User.objects.get(pk=pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+
+            messages.success(request,"Password reset successfully")
+            return redirect('login')
+        
+        else:
+            messages.error(request, "Password does not match")
+            return redirect('reset_password')
+                                 
+    return render(request, 'accounts/reset_password.html')
+
 
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
