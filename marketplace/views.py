dee43869cf00912d6caa40a27be41017755eefa1
amarendra <imamarendra.singh@gmail.com>
2024-08-14 22:20:48 +0530

    cart

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 from vendor.models import Vendor
+from menu.models import Category, FoodItem
+from django.db.models import Prefetch
+from marketplace.models import Cart
 # Create your views here.
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -11,5 +15,47 @@ def marketplace(request):
     return render(request, 'marketplace/listing.html', context)
 
 def vendor_detail(request,vendor_slug):
-    
-    return render(request,'marketplace/vendor_detail.html')
+    vendor= get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset=FoodItem.objects.filter(is_available=True)
+        )
+    )
+
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
+
+    context = {
+        'vendor':vendor,
+        'categories':categories,
+        'cart_items':cart_items
+    }
+    return render(request,'marketplace/vendor_detail.html', context)
+
+
+def add_to_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with')== 'XMLHttpRequest':
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                try:
+                    check_cart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    check_cart.quantity +=1
+                    check_cart.save()
+
+                except:
+                    check_cart = Cart.objects.create(user=request.user, fooditem=fooditem, qauntity = 1)
+                    return JsonResponse({'status':'Sucess', 'message':'Food added to cart!'})
+
+
+            except:
+                return JsonResponse({'status':'Failed', 'message':'This food does not exist!'})
+ 
+        else:
+
+            return JsonResponse({'status':'Failed', 'message':'Invaild reuest!'})
+    else:
+        return JsonResponse({'status':'Failed', 'message':'Please login to continue'})
