2d015f27ebdbc1a8e3c08d70faa555671cde95d3
amarendra <imamarendra.singh@gmail.com>
2024-09-16 19:18:45 +0530

    search

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,5 +1,6 @@
 from vendor.models import Vendor
 from django.conf import settings
+from marketplace.models import Cart, FoodItem
 
 def get_vendor(request):
     try:
@@ -10,3 +11,17 @@ def get_vendor(request):
 
 def get_google_api(request):
     return{'GOOGLE_API_KEY':settings.GOOGLE_API_KEY}
+
+
+def get_cart_amounts(request):
+    subtotal = 0
+    tax = 0
+    grand_total = 0
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user = request.user)
+        for item in cart_items:
+            fooditem = FoodItem.objects.get(pk=item.fooditem.id)
+            subtotal += (fooditem.price * item.quantity)
+
+        grand_total = subtotal + tax
+    return dict(subtotal=subtotal, tax=tax, grand_total = grand_total)
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from vendor.models import Vendor
 from menu.models import Category, FoodItem
 from django.db.models import Prefetch
 from marketplace.models import Cart
+
+from marketplace.context_processors import get_cart_counter
+
 # Create your views here.
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -45,10 +48,11 @@ def add_to_cart(request, food_id):
                     check_cart = Cart.objects.get(user=request.user, fooditem=fooditem)
                     check_cart.quantity +=1
                     check_cart.save()
+                    return JsonResponse({'status':'Sucess', 'message':'Increased the cart quantity', 'cart_counter':get_cart_counter(request)})
 
                 except:
                     check_cart = Cart.objects.create(user=request.user, fooditem=fooditem, qauntity = 1)
-                    return JsonResponse({'status':'Sucess', 'message':'Food added to cart!'})
+                    return JsonResponse({'status':'Sucess', 'message':'Food added to cart!','cart_counter':get_cart_counter(request)})
 
 
             except:
@@ -59,3 +63,61 @@ def add_to_cart(request, food_id):
             return JsonResponse({'status':'Failed', 'message':'Invaild reuest!'})
     else:
         return JsonResponse({'status':'Failed', 'message':'Please login to continue'})
+    
+
+def decrease_cart(request, food_id):
+    
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with')== 'XMLHttpRequest':
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                try:
+                    check_cart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    if check_cart > 1:
+                        check_cart.quantity -=1
+                        check_cart.save()
+                    else:
+                        check_cart.delete()
+                        check_cart.quantity = 0
+                    return JsonResponse({'status': 'Success', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
+                except:
+                    return JsonResponse({'status': 'Failed', 'message': 'You do not have this item in your cart!'})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+        
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
+
+
+def cart(request):
+    cart_items = Cart.objects.filter(user=request.user)
+    context = {
+        'carrt_items': cart_items,
+    }
+    return render(request, 'marketplace/cart.html', context)
+
+def delete_cart(request, cart_id):
+    if request.user.is_authencitated:
+        if request.is_ajax():
+            try:
+                cart_item = Cart.objects.get(user = request.user, id = cart_id)
+                if cart_item:
+                    cart_item.delete()
+                    return JsonResponse({'status':'Success', 'message':'Cart item has deleted!', 'cart_counter': get_cart_counter(request)})
+            except:
+                return JsonResponse({'status':'Failed', 'message':'Cart items does not exist'})
+        else:
+            return JsonResponse({'status':'Failed', 'message':'Invalid request!'})
+        
+
+def search(request):
+    if not 'address' in request.GET:
+        return redirect ('marketplace')
+    else:
+        address = request.GET['address']
+        latitude = request.GET['lat']
+        longitude = request.GET['lng']
+        radius = request.GET['radius']
+        keyword = request.GET['keyword']
